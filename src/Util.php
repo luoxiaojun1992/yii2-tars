30e200e3cc07b042960406a58a2a0b7da218ebd0
luoxiaojun <luoxiaojun@dodoca.com>
2019-06-13 14:39:37 +0800

    init commit

---
+++ src/Util.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lxj\Laravel\Tars;
+
+use Tars\Utils;
+
+class Util
+{
+    public static function parseTarsConfig($cfg)
+    {
+        $hostname = gethostname();
+        $tarsConfig = Utils::parseFile($cfg);
+        $tarsServerConf = $tarsConfig['tars']['application']['server'];
+        $port = $tarsServerConf['listen'][0]['iPort'];
+        $appName = $tarsServerConf['app'];
+        $serverName = $tarsServerConf['server'];
+        return [$hostname, $port, $appName, $serverName];
+    }
+
+    public static function app()
+    {
+        return \Yii::$app;
+    }
+}
