b6e114789a1293a686e2ee4190d18ec4d9c50581
luoxiaojun <luoxiaojun@dodoca.com>
2019-06-27 11:17:05 +0800

    update tars server

--- src/Commands/TarsController.php
+++ src/Commands/TarsController.php
@@ -26,12 +26,8 @@ class TarsController extends Controller
         $class->run();
     }
 
-    public function actionPublish($tag = 'tars.http')
+    public function actionPublish()
     {
-        if (!in_array($tag, ['tars.http', 'tars.tars'])) {
-            $this->stderr('Invalid tag.');
-        }
-
         $basePath = Util::app()->getBasePath();
         $tarsServantDir = $basePath . '/tars/servant';
         $tarsServantImplDir = $basePath . '/tars/impl';
@@ -41,34 +37,19 @@ class TarsController extends Controller
         $this->ensureDirExisted($tarsServantImplDir);
         $this->ensureDirExisted($tarsCservantDir);
 
-        $publicResources = [
+        $this->publishResource([
             __DIR__ . '/../index.php' => $basePath . '/index.php',
             __DIR__ . '/../Tars/cservant/.gitkeep' => $tarsCservantDir . '/.gitkeep',
             __DIR__ . '/../services.php' => $basePath . '/services.php',
             __DIR__ . '/../../tars/tars.proto.php' => $basePath . '/../tars/tars.proto.php',
-        ];
+            __DIR__ . '/../scripts/tars2php.sh' => $basePath . '/../scripts/tars2php.sh',
+            __DIR__ . '/../Tars/servant/.gitkeep' => $tarsServantDir . '/.gitkeep',
+            __DIR__ . '/../Tars/impl/.gitkeep' => $tarsServantImplDir . '/.gitkeep',
+        ]);
 
-        $resources = [
-            'tars.http' => $publicResources,
-            'tars.tars' => array_merge($publicResources, [
-                __DIR__ . '/../scripts/tars2php.sh' => $basePath . '/../scripts/tars2php.sh',
-                __DIR__ . '/../Tars/servant/.gitkeep' => $tarsServantDir . '/.gitkeep',
-                __DIR__ . '/../Tars/impl/.gitkeep' => $tarsServantImplDir . '/.gitkeep',
-            ]),
-        ];
-
-        $this->publishResource($resources[$tag]);
-
-        $config = [
-            'tars.http' => [
-                __DIR__ . '/../config/tars.http.php' => $basePath . '/config/params.php',
-            ],
-            'tars.tars' => [
-                __DIR__ . '/../config/tars.tars.php' => $basePath . '/config/params.php',
-            ],
-        ];
-
-        $this->publishConfig($config[$tag]);
+        $this->publishConfig([
+            __DIR__ . '/../config/tars.php' => $basePath . '/config/params.php',
+        ]);
 
         $this->stdout('Published.' . PHP_EOL);
     }
